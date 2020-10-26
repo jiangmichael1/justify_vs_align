54fbd2a879dd038a31a41794d6915530cd7be3c8
jiangmichael1 <jiangm1@yahoo.com>
2020-10-26 14:35:35 -0400

    adding functionality to buttons

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// CSS Properties Functionality
+
+const justifyList = document.querySelector('.justify-list')
+const alignList = document.querySelector('.align-list')
+
+const justifyContent = document.querySelector('#justify-content')
+const justifyItems = document.querySelector('#justify-items')
+const justifySelf = document.querySelector('#justify-self')
+
+const alignContent = document.querySelector('#align-content')
+const alignItems = document.querySelector('#align-items')
+const alignSelf = document.querySelector('#align-self')
+
+
+// Toggling Axis Display
+
 const axisCheck = document.querySelector('#axis') 
 const mainAxis = document.querySelector('.main-axis')
 const crossAxis = document.querySelector('.cross-axis')
