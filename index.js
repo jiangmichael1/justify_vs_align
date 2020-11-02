d699cc4bdce1ca44fdaa6ee0a3f14fd87bc1a48a
jiangmichael1 <jiangm1@yahoo.com>
2020-11-01 20:32:00 -0500

    working on radio button functionality

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 // CSS Properties Functionality
 
-const justifyList = document.querySelector('.justify-list')
-const alignList = document.querySelector('.align-list')
+const justifyList = document.querySelector('.justify-list');
+const alignList = document.querySelector('.align-list');
 
-const justifyContentProp = document.querySelector('#justify-content')
-const justifyItemsProp = document.querySelector('#justify-items')
-const justifySelfProp = document.querySelector('#justify-self')
+const parentRadio = document.querySelector('#parent-radio');
+const childRadio = document.querySelector('#child-radio');
 
-const alignContentProp = document.querySelector('#align-content')
-const alignItemsProp = document.querySelector('#align-items')
-const alignSelfProp = document.querySelector('#align-self')
+const justifyContentProp = document.querySelector('#justify-content');
+const justifyItemsProp = document.querySelector('#justify-items');
+const justifySelfProp = document.querySelector('#justify-self');
 
-const parentBox = document.querySelector('.parent')
-const childBox = document.querySelector('.child')
+const alignContentProp = document.querySelector('#align-content');
+const alignItemsProp = document.querySelector('#align-items');
+const alignSelfProp = document.querySelector('#align-self');
+
+const parentBox = document.querySelector('.parent');
+const childBox = document.querySelector('.child');
 
 
 // Toggling Axis Display
 
-const axisCheck = document.querySelector('#axis') 
-const mainAxis = document.querySelector('.main-axis')
-const crossAxis = document.querySelector('.cross-axis')
+const axisCheck = document.querySelector('#axis') ;
+const mainAxis = document.querySelector('.main-axis');
+const crossAxis = document.querySelector('.cross-axis');
 
 axisCheck.addEventListener('change', function() {
-    if (axisCheck.checked){
-        mainAxis.style.display = 'block'
-        crossAxis.style.display = 'block'
+    if (!axisCheck.checked){
+        mainAxis.style.display = 'none';
+        crossAxis.style.display = 'none';
     } else {
-        mainAxis.style.display = 'none'
-        crossAxis.style.display = 'none'
+        mainAxis.style.display = 'block';
+        crossAxis.style.display = 'block';
     }
 })
+
+console.log(parentRadio.checked)
 
